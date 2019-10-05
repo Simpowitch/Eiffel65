96937afe9b4b374df219beb5f8206e28a40f244e
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2019-10-05 17:42:10 +0200

    Improvement to node script
    Now selects the newly created node and also positions the new node between the connected nodes

--- Assets/Scripts/Editor/PathNodeEditor.cs
+++ Assets/Scripts/Editor/PathNodeEditor.cs
@@ -18,14 +18,17 @@ public class PathNodeEditor : Editor
             PathNode newNode = Instantiate(Resources.Load<PathNode>("Prefabs/Road/Pathnode"), myPathNode.transform.position, myPathNode.transform.rotation);
             myPathNode.AddConnectedNode(newNode);
             newNode.transform.SetParent(myPathNode.transform.parent);
+            Selection.activeGameObject = newNode.transform.gameObject;
         }
 
         if (GUILayout.Button("Replace connected node with new node"))
         {
             PathNode newNode = Instantiate(Resources.Load<PathNode>("Prefabs/Road/Pathnode"), myPathNode.transform.position, myPathNode.transform.rotation);
+            newNode.transform.position = (myPathNode.transform.position + myPathNode.GetPathNodes()[0].transform.position) / 2;
             newNode.AddConnectedNode(myPathNode.GetPathNodes());
             myPathNode.ReplaceConnectedNode(newNode);
             newNode.transform.SetParent(myPathNode.transform.parent);
+            Selection.activeGameObject = newNode.transform.gameObject;
         }
     }
 }
