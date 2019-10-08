b6cba1b336d922b4efb4eb732c695a5bd98388f3
peanutpowah <samuel.zelenka@hotmail.com>
2019-10-08 11:12:29 +0200

    Dialog Editor first iplementation
    Dialog editor create visual nodes
    Connect nodes
    classes for Scenarios created

---
+++ Assets/Scripts/DialogScenario/Scenario.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario
+{
+    List<Dialog> dialogs;
+}
+
+public class Dialog
+{
+
+    public string location;
+    public string dialogText;
+    List<Choice> choices;
+    Dialog(string location, string dialogText, List<Choice> choices)
+    {
+        this.location = location;
+        this.dialogText = dialogText;
+        this.choices = choices;
+    }
+
+}
+
+public class Choice
+{
+    string choiceText;
+    Dialog nextDialog;
+
+    Choice(string choiceText, Dialog nextDialog)
+    {
+        this.choiceText = choiceText;
+        this.nextDialog = nextDialog;
+    }
+}
