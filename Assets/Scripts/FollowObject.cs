374ec6584455450fe6c538ed335a4a4a5839455a
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-01-30 22:42:35 +0100

    minimap fixes

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -6,14 +6,15 @@ public class FollowObject : MonoBehaviour
 {
     public Transform objectToFollow = null;
 
-    public float height = 50;
+    float height = 50;
+    float offset = 60;
 
     // Update is called once per frame
     void Update()
     {
         if (objectToFollow)
         {
-            this.transform.position = objectToFollow.transform.position + new Vector3(0, height, 0);
+            this.transform.position = objectToFollow.transform.position + new Vector3(0, height, 0) + objectToFollow.transform.forward * offset;
             this.transform.eulerAngles = new Vector3(90, 0, -objectToFollow.eulerAngles.y);
         }
     }
