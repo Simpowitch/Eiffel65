41a2affee9fd9443cc627df678b5c8f896a1b14f
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-01-30 21:34:47 +0100

    Easy minimap

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform objectToFollow = null;
+
+    public float height = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectToFollow)
+        {
+            this.transform.position = objectToFollow.transform.position + new Vector3(0, height, 0);
+            this.transform.eulerAngles = new Vector3(90, 0, -objectToFollow.eulerAngles.y);
+        }
+    }
+}
